4cd51053fa0f693a85353013d6c28c7b24a78eda
jogeuncheol <blueqwerty@naver.com>
2022-07-07 14:54:24 +0900

    [add][func] get_face_bbox: face_detection의 face bounding box 시작, 끝 좌표를 얻음.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 create: 2022-06-25
-edit:   2022-06-29
+edit:   2022-07-07
 author: github.com/jogeuncheol
 """
 
@@ -13,12 +13,12 @@ mp_drawing = mp.solutions.drawing_utils
 
 # For static images:
 def static_images():
-    IMAGE_FILES = ['image/01.jpg', 'image/02.jpg']
+    image_files = ['image/01.jpg', 'image/02.jpg']
     with mp_face_detection.FaceDetection(
             model_selection=1,
             min_detection_confidence=0.5
     ) as face_detection:
-        for idx, file in enumerate(IMAGE_FILES):
+        for idx, file in enumerate(image_files):
             image = cv2.imread(file)
             # Convert the BGR image to RGB and process it with MediaPipe Face Detection.
             results = face_detection.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
@@ -54,20 +54,46 @@ def web_cam():
             results = face_detection.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
             # Draw face detections of each face.
-            if not results.detections:
-                continue
             annotated_image = image.copy()
-            for detection in results.detections:
-                print('Nose tip:')
-                print(mp_face_detection.get_key_point(
-                    detection, mp_face_detection.FaceKeyPoint.NOSE_TIP))
-                mp_drawing.draw_detection(annotated_image, detection)
+            if results.detections:
+                for detection in results.detections:
+                    mp_drawing.draw_detection(annotated_image, detection)
+                    face_bbox = get_face_bbox(annotated_image, detection)
+                    print(face_bbox)
+                    cv2.rectangle(annotated_image, face_bbox[0], face_bbox[1],
+                                  (0, 0, 255), 1)
             # cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
             cv2.imshow('image', annotated_image)
             # cv2.waitKey(0)
             if cv2.waitKey(5) & 0xFF == 27:
                 break
     cam.release()
+
+
+def get_face_bbox(image, detection):
+    """
+    Args:
+        image: A three channel BGR image represented as numpy ndarray.
+        detection: A detection proto message to be annotated on the image.
+    Return: list[start(x, y), end(x + width, y + height)]:
+    """
+    if image.shape[2] != mp_drawing._BGR_CHANNELS:
+        raise ValueError('Input image must contain three channel bgr data.')
+    image_rows, image_cols, _ = image.shape
+    relative_bounding_box = detection.location_data.relative_bounding_box
+    rect_start_point = mp_drawing._normalized_to_pixel_coordinates(
+        relative_bounding_box.xmin,
+        relative_bounding_box.ymin,
+        image_cols,
+        image_rows
+    )
+    rect_end_point = mp_drawing._normalized_to_pixel_coordinates(
+        relative_bounding_box.xmin + relative_bounding_box.width,
+        relative_bounding_box.ymin + relative_bounding_box.height,
+        image_cols,
+        image_rows
+    )
+    return [rect_start_point, rect_end_point]
 
 
 def set_roi():
